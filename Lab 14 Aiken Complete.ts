6758f14bd87ded5205a1915ed63f1a27f49c1eed
Vu Nam <164347541+SapphireRolex@users.noreply.github.com>
2025-08-03 10:14:28 +0700

    Update Lab 14 Aiken Complete.ts

--- Lab 14 Aiken Complete.ts
+++ Lab 14 Aiken Complete.ts
@@ -77,64 +77,64 @@ if (!payment_hash) {
 
 //------------MINT-NFT--------------------
 
-// //===============Đặt tên token và setting phí trả về platform==================
-// const token_name = fromText("BK02_31");
-// const fee_value = 10000000n;
-// // const payment_credential = paymentCredentialOf("addr_test1qqew6jaz63u389gwnp8w92qntetzxs6j9222pn4cnej672vazs7a6wnrseqggj4d4ur43yq9e23r4q0m879t7efyhzjq8mvzua").hash;
-// // 32ed4ba2d47913950e984ee2a8135e562343522a94a0ceb89e65af29
+//===============Đặt tên token và setting phí trả về platform==================
+const token_name = fromText("BK02_31");
+const fee_value = 10000000n;
+// const payment_credential = paymentCredentialOf("addr_test1qqew6jaz63u389gwnp8w92qntetzxs6j9222pn4cnej672vazs7a6wnrseqggj4d4ur43yq9e23r4q0m879t7efyhzjq8mvzua").hash;
+// 32ed4ba2d47913950e984ee2a8135e562343522a94a0ceb89e65af29
 
 
-// //------Kê khai thông tin địa chỉ nhận fee của platform---------
-// const payment_credential = Addresses.inspect(
-//   "addr_test1qz22wkszyt9kgqhk24670xz7ehs9tvlhq079rzy5vyekn5p5x065mhvs0z9p2gymxgguy3w0v5qnk39klvaapeqla97qsdltfy"
-// ).payment?.hash;
-// console.log(payment_credential);
+//------Kê khai thông tin địa chỉ nhận fee của platform---------
+const payment_credential = Addresses.inspect(
+  "addr_test1qz22wkszyt9kgqhk24670xz7ehs9tvlhq079rzy5vyekn5p5x065mhvs0z9p2gymxgguy3w0v5qnk39klvaapeqla97qsdltfy"
+).payment?.hash;
+console.log(payment_credential);
 
-// //-------Gọi và truyền thông tin định dạng của biến multiparams-----------
-// const validator = await readValidator();
-// const Params = [Data.Bytes(), Data.Integer(), Data.Bytes()];
-// const parameterized_script = lucid.newScript(
-//   {
-//     type: "PlutusV3",
-//     script: validator.script,
-//   },
-//   [token_name, fee_value, payment_credential],
-//   Params
-// );
+//-------Gọi và truyền thông tin định dạng của biến multiparams-----------
+const validator = await readValidator();
+const Params = [Data.Bytes(), Data.Integer(), Data.Bytes()];
+const parameterized_script = lucid.newScript(
+  {
+    type: "PlutusV3",
+    script: validator.script,
+  },
+  [token_name, fee_value, payment_credential],
+  Params
+);
 
-// //------Tạo các thông tin về địa chỉ script và tạo policy cùng tên cho NFT----------
-// const scriptAddress = parameterized_script.toAddress();
-// console.log(`Địa chỉ Parameterized script là: ${scriptAddress}`);
-// const policyId = parameterized_script.toHash();
-// const unit = policyId + fromText("BK02_31");
+//------Tạo các thông tin về địa chỉ script và tạo policy cùng tên cho NFT----------
+const scriptAddress = parameterized_script.toAddress();
+console.log(`Địa chỉ Parameterized script là: ${scriptAddress}`);
+const policyId = parameterized_script.toHash();
+const unit = policyId + fromText("BK02_31");
 
-// const mintRedeemer = Data.to(new Constr(0, []));
-// // const mintRedeemer = Data.void()
-// const tx = await lucid
-//   .newTx()
-//   .mint({ [unit]: 1n }, mintRedeemer)
-//   .payTo(
-//     "addr_test1qz22wkszyt9kgqhk24670xz7ehs9tvlhq079rzy5vyekn5p5x065mhvs0z9p2gymxgguy3w0v5qnk39klvaapeqla97qsdltfy",
-//     { lovelace: 10000000n }
-//   )
-//   .attachScript(parameterized_script)
-//   .commit();
+const mintRedeemer = Data.to(new Constr(0, []));
+// const mintRedeemer = Data.void()
+const tx = await lucid
+  .newTx()
+  .mint({ [unit]: 1n }, mintRedeemer)
+  .payTo(
+    "addr_test1qz22wkszyt9kgqhk24670xz7ehs9tvlhq079rzy5vyekn5p5x065mhvs0z9p2gymxgguy3w0v5qnk39klvaapeqla97qsdltfy",
+    { lovelace: 10000000n }
+  )
+  .attachScript(parameterized_script)
+  .commit();
 
-// const signedTx = await tx.sign().commit();
-// await Deno.writeTextFile("Mint-signedTx.cbor", signedTx);
-// const txHash = await signedTx.submit();
-// console.log(`A NFT was mint at tx:    https://preview.cexplorer.io/tx/${txHash} `);
+const signedTx = await tx.sign().commit();
+await Deno.writeTextFile("Mint-signedTx.cbor", signedTx);
+const txHash = await signedTx.submit();
+console.log(`A NFT was mint at tx:    https://preview.cexplorer.io/tx/${txHash} `);
 
-// //===============Đọc mã CBOR của SC  ============================
-// async function readValidator(): Promise<SpendingValidator> {
-//   const validator = JSON.parse(await Deno.readTextFile("plutus.json")).validators[0];
-//   return {
-//     type: "PlutusV3",
-//     script: toHex(cbor.encode(fromHex(validator.compiledCode))),
-//   };
-// }
+//===============Đọc mã CBOR của SC  ============================
+async function readValidator(): Promise<SpendingValidator> {
+  const validator = JSON.parse(await Deno.readTextFile("plutus.json")).validators[0];
+  return {
+    type: "PlutusV3",
+    script: toHex(cbor.encode(fromHex(validator.compiledCode))),
+  };
+}
 
-//---------------BURN-NFT------------------------
+---------------BURN-NFT------------------------
 
 const token_name = fromText("BK02_31");
 const fee_value = 10000000n;
